91725235564345ac073b0a859c4ab7a2a0fab377
WestleyK <westley@sylabs.io>
2018-07-30 23:32:46 +0000

    added auto-complete.sh for main script

---
+++ auto-complete.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Created by: WestleyK
+# Email: westley@sylabs.io
+# Date: Jul 30, 2018
+# version-1.0.0
+# https://github.com/WestleyK/easy-clone
+#
+#
+# MIT License
+#
+# Copyright (c) 2018 WestleyK
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+
+_auto-complete() {
+	OPTION=$( cat ~/easy-clone/option-files )
+	local cur=${COMP_WORDS[COMP_CWORD]}
+	COMPREPLY=( $(compgen -W "$OPTION" -- $cur) )
+}
+complete -F _auto-complete hubget
+
+
+
+#
+# End script
+#
+
+
